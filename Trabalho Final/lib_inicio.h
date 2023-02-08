4261631df1a36107c6eed0f43a3433bf51deb82f
Maisa <117362944+maisamoren@users.noreply.github.com>
2023-02-08 18:28:07 -0300

    Add files via upload

---
+++ Trabalho Final/lib_inicio.h
@@ -0,0 +1,51 @@
+#ifndef __LIB_INICIO__
+#define __LIB_INICIO__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <time.h>
+
+#define LIN 7
+#define COL 21
+#define BORDA '#'
+#define PAREDEL '|'
+#define PAREDESI '-'
+#define VAZIO ' '
+#define ENTREGA '@'
+#define CHAPEIRO '&'
+#define LIXO 'o' 
+#define HAMBURGER 'H' 
+#define PBAIXO 'p'
+#define PCIMA 'P'
+#define QUEIJO 'Q'
+#define SALADA 'S' 
+#define FRITAS 'F' 
+#define REFRI 'R'
+#define CHAPEIRO_X_INIT 2
+#define CHAPEIRO_Y_INIT 10
+
+typedef struct coordenadas{
+    int x, //linhas
+        y; //colunas
+}COORDENADAS;
+
+// void entrada (int *p);
+
+// void saida ();
+
+// void menu ();
+
+int bordas (int i, int j);
+
+int teto (int i, int j);
+
+int lado (int i, int j);
+
+char** fazer_mapa ();
+
+void colocar_chapeiro_mapa (char **mapa, COORDENADAS *chapeiro);
+
+void imprime(char **mapa);
+
+#endif
