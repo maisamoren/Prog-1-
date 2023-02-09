8e0503689fc830abd038d2919a4ca1652ebc1989
Maisa <117362944+maisamoren@users.noreply.github.com>
2023-02-09 11:13:14 -0300

    Add files via upload

---
+++ Trabalho Final/lib_legenda.h
@@ -0,0 +1,19 @@
+#ifndef __LIB_LEGENDA__
+#define __LIB_LEGENDA__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <time.h>
+
+void entrada ();
+
+void saida ();
+
+void menu ();
+
+void pedidos ();
+
+void help ();
+
+#endif
